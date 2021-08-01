f96fb2f1c1b2edd92174e91e90504b441f75d282
marcos <marcos.rib.sousa@gmail.com>
2021-08-01 17:22:11 -0300

    fix: add text about

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import Layout from "../components";
 import styles from '../../styles/Home/Home.module.css'
 export default function Home() {
-  {/*<img src="/home-image.jpeg" className={styles.img} alt="kknknkk"/>*/}
+  {/*<img src="/home-image.jpeg" className={styles.img} alt="kknknkk"/>*/ }
   return (
- <div className={styles.img}>
+    <div className={styles.img}>
 
- </div>
-   
+    </div>
+
   )
 }
