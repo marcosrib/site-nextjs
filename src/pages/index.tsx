3139b928c577fa259c4d0578afa99763f7ff6312
marcosrib <marcos.rib.sousa@gmail.com>
2022-06-21 13:44:32 -0300

    feat: add banner home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,17 @@
 import styles from '../../styles/Home/Home.module.css'
-import Image from 'next/image';
+
 export default function Home() {
+  
   {/*<img src="/home-image.jpeg" className={styles.img} alt="kknknkk"/>*/ }
   return (
-    <div className={styles.img}>
-      {/*  <Image src="/home-image.png"
-        alt="kknknkk"
-        height={"100vh"}
-        width="100vw"
-  /> */}
-
-      <div className={styles.cardContainer}>
-        <h1 id={styles.title}>TERAPIA ONLINE NO CONFORTO DA SUA CASA</h1>
-        <div className={styles.textContent}>
-          <h3 id={styles.subTitle}>Thais Gomes Oliveira</h3>
-          <span id={styles.text}> Psicóloga CRP 06/154046 (Terapeuta Cognitiva Comportamental)
-            Atendimento online</span>
-        </div>
-        <span id={styles.text2}>Uma alma humana, comprometida, determinada, de coração grato e motivada.</span>
-        <br></br>
-        <span id={styles.text2}>"Apaixonada pelo que faz, tem como essência trabalhar ajudando as pessoas na busca do seu PROPÓSITO, QUALIDADE DE VIDA, SAÚDE MENTAL e sua MELHOR VERSÃO!</span>
+    <div className={styles.container}>
+      <div className={styles.img}/>
+   
+        <div className={styles.textImageContent}>
+          <text className={styles.title}>O apoio que você precisa</text>
+          <text className={styles.subTitle}>Está a alguns cliques</text>
+          </div>
+        
       </div>
-
-    </div >
-
   )
 }
