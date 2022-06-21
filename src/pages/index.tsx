4214e4ca8a0be15137ca37e9f4b66de28e017c9d
marcosrib <marcos.rib.sousa@gmail.com>
2022-06-21 14:18:15 -0300

    fix: Alimento texto home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,21 @@
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from "next/router";
 import styles from '../../styles/Home/Home.module.css'
 
 export default function Home() {
-  
+  const router = useRouter();
   {/*<img src="/home-image.jpeg" className={styles.img} alt="kknknkk"/>*/ }
   return (
     <div className={styles.container}>
       <div className={styles.img}/>
    
         <div className={styles.textImageContent}>
-          <text className={styles.title}>O apoio que você precisa</text>
-          <text className={styles.subTitle}>Está a alguns cliques</text>
+          <span className={styles.title}>O apoio que você precisa</span>
+          <span className={styles.subTitle}>Está a alguns cliques</span>
+          <div className={styles.buttonAgendar} >
+            <Link href='/'><a  className={router.pathname == "/" ? styles.active : ""}>Home</a></Link>
+          </div>
           </div>
         
       </div>
