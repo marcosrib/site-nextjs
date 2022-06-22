5555f1366ed8d501c62343887e5a69c1f1670722
marcosrib <marcos.rib.sousa@gmail.com>
2022-06-22 14:19:13 -0300

    feat: Melhorado pagina home desktop e mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,15 +8,28 @@ export default function Home() {
   {/*<img src="/home-image.jpeg" className={styles.img} alt="kknknkk"/>*/ }
   return (
     <div className={styles.container}>
-      <div className={styles.img}/>
+      <div className={styles.content}>
    
         <div className={styles.textImageContent}>
-          <span className={styles.title}>O apoio que você precisa</span>
+          <span className={styles.title}>Terapia Online No Conforto Da Sua Casa</span>
           <span className={styles.subTitle}>Está a alguns cliques</span>
           <div className={styles.buttonAgendar} >
-            <Link href='/'><a  className={router.pathname == "/" ? styles.active : ""}>Home</a></Link>
+            <Link href='/contato'><a  className={router.pathname == "/contato" ? styles.active : ""}>Contato</a></Link>
           </div>
-          </div>
+        </div>
+        </div>
+      <div className={styles.containerSobre}>
+        <img src="/fotothais.jpeg" className={styles.img}/>
+        <div className={styles.descriptionContent}>
+        <h3 className={styles.nameTitle}>Thais Gomes Oliveira</h3>
+        <span className={styles.description} > Psicóloga CRP 06/154046 (Terapeuta Cognitiva Comportamental)
+            Atendimento online. <br></br>
+            Uma alma humana, comprometida, determinada, de coração grato e motivada.
+            paixonada pelo que faz, tem como essência trabalhar ajudando as pessoas na busca do seu <span className={styles.descriptionStrong}> PROPÓSITO, QUALIDADE DE VIDA, SAÚDE MENTAL </span> e sua <span className={styles.descriptionStrong}>MELHOR VERSÃO!</span>
+            </span>
+        </div>
+     
+      </div>
         
       </div>
   )
